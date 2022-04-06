9a6546efa3714a600cc28f7f96fc8005fc1923cf
luiscordovaxepelin <luis.cordova@xepelin.com>
2022-04-06 10:06:50 -0500

    load project

---
+++ Api.Business/WeatherOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Api.Business
+{
+    public class WeatherOperation
+    {
+
+        public  string[] Summaries () {
+            return new string[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        }
+
+    }
+}
---
+++ AppApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Api.Business;
+using Api.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly WeatherOperation operations;
+
+         private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            operations = new WeatherOperation();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var Summaries = operations.Summaries();
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
